7d9b4761f6e4120e71c3b3bfd145e791681e3371
Dan <djcavanagh@gmail.com>
2017-12-11 12:03:58 -0500

    httpPut test refactored test_utils to reuse code

--- test/test_utils.ts
+++ test/test_utils.ts
@@ -17,31 +17,8 @@ export function httpGet(
     requestHeaders?: ([string, string][]) | null,
     expectedResponseHeaders?: ([string, string][]) | null
 ) {
-    return new Promise<request.Response>((resolve, reject) => {
-
-        let r = request(app).get(url)
-                            .expect(expectedResponseCode);
-
-        if (requestHeaders) {
-            requestHeaders.forEach(h => {
-                r = r.set(h[0], h[1]);
-            });
-        }
-
-        if (expectedResponseHeaders) {
-            expectedResponseHeaders.forEach(h => {
-                r = r.expect(h[0], h[1]);
-            });
-        }
-
-        r.end(function(err, res) {
-            if (err) {
-                reject(err);
-            }
-            resolve(res);
-        });
-
-    });
+    let data = "";
+    return handleRequest(new RequestConfig("get", app, url, expectedResponseCode, data, requestHeaders, expectedResponseHeaders));
 }
 
 export function httpPost<TData>(
@@ -52,25 +29,67 @@ export function httpPost<TData>(
     requestHeaders?: ([string, string][]) | null,
     expectedResponseHeaders?: ([string, string][]) | null
 ) {
+    return handleRequest(new RequestConfig("post", app, url, expectedResponseCode, data, requestHeaders, expectedResponseHeaders));
+}
+
+export function httpPut<TData>(
+    app: express.Application,
+    url: string,
+    data: TData,
+    expectedResponseCode: number,
+    requestHeaders?: ([string, string][]) | null,
+    expectedResponseHeaders?: ([string, string][]) | null
+) {
+    return handleRequest(new RequestConfig("put", app, url, expectedResponseCode, data, requestHeaders, expectedResponseHeaders));
+}
+
+export function httpDelete<TData>(
+    app: express.Application,
+    url: string,
+    expectedResponseCode: number,
+    requestHeaders?: ([string, string][]) | null,
+    expectedResponseHeaders?: ([string, string][]) | null
+) {
+    let data = "";
+    return handleRequest(new RequestConfig("delete", app, url, expectedResponseCode, data, requestHeaders, expectedResponseHeaders));
+}
+
+export class RequestConfig<TData> {
+    constructor(public type: string,
+        public app: express.Application,
+        public url: string,
+        public expectedResponseCode: number,
+        public data?: TData,
+        public requestHeaders?: ([string, string][]) | null,
+        public expectedResponseHeaders?: ([string, string][]) | null,
+    ) { }
+}
+
+export async function handleRequest<TData>(requestConfig: RequestConfig<TData>) {
     return new Promise<request.Response>((resolve, reject) => {
+        let r: any;
+        if (requestConfig.data) {
+            r = request(requestConfig.app)[requestConfig.type](requestConfig.url)
+                .send(requestConfig.data)
+                .expect(requestConfig.expectedResponseCode);
+        } else {
+            r = request(requestConfig.app)[requestConfig.type](requestConfig.url)
+                .expect(requestConfig.expectedResponseCode);
+        }
 
-        let r = request(app).post(url)
-                            .send(data)
-                            .expect(expectedResponseCode);
-
-        if (requestHeaders) {
-            requestHeaders.forEach(h => {
+        if (requestConfig.requestHeaders) {
+            requestConfig.requestHeaders.forEach(h => {
                 r = r.set(h[0], h[1]);
             });
         }
 
-        if (expectedResponseHeaders) {
-            expectedResponseHeaders.forEach(h => {
+        if (requestConfig.expectedResponseHeaders) {
+            requestConfig.expectedResponseHeaders.forEach(h => {
                 r = r.expect(h[0], h[1]);
             });
         }
 
-        r.end(function(err, res) {
+        r.end(function (err: any, res: any) {
             if (err) {
                 reject(err);
             }
